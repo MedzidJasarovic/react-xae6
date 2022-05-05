55402fd1e0f8bc9feba37494f39e353ed2cf5384
MedzidJasarovic <medzidjasarevic1212@gmail.com>
2022-05-05 13:55:16 +0200

    commit8

--- src/components/CounterFunctional.js
+++ src/components/CounterFunctional.js
@@ -6,7 +6,7 @@ function Example() {
   return (
     <div>
       <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>Click me</button>
+      <button onClick={() => setCount(count + 1)}>Povecaj</button>
     </div>
   );
 }
